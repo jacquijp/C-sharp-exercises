96014e23c2137769003144eec0442ae606d885fc
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-30 15:18:57 +0200

    Variable changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,17 +2,23 @@
 
 //This code calculates the area of a rectangle or quarter
 
-//Declaration of variables
-int sideA;
-int sideB;
-int result;
+//Declaration of variables explicitly
+//double sideA;
+//double sideB;
+//double result;
+
+//Declararion of varibales implicit
+var sideA = 0.0;
+var sideB = 0.0;
+var result = 0.0;
 
 Console.WriteLine("Rectangle and quarter area calculator");
-Console.WriteLine("Type the side A value: ");
-sideA = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Type the side B value");
-sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type the side A value: ");
+sideA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Type the side B value: ");
+sideB = Convert.ToDouble(Console.ReadLine());
 
 result = sideA * sideB;
 Console.WriteLine("Side A is: " + sideA + " Side B is: " + " Area = " + result);
