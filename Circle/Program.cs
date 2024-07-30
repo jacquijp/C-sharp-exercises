a90add6a4c00ea29c01c4ecd5d389ed6b4bbe033
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-30 15:40:21 +0200

    Circle area code added - version 1

---
+++ Circle/Program.cs
@@ -0,0 +1,14 @@
+﻿//Code for solving circle area
+
+var radius = 0d; //0 double -- implicit initial declaration
+var result = 0d; // if i use 0 only, it can be mistaken as an int
+
+Console.WriteLine("Welcome!");
+Console.WriteLine("Cirle area calculator");
+//area = Pi * (radious*radius)
+Console.WriteLine("Type radious value");
+radius = Convert.ToDouble(Console.ReadLine());
+
+result = Math.PI * radius * radius;
+
+Console.WriteLine("Circle area is: " + result);
