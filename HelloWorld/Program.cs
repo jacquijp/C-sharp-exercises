2cea38f59457d1bc9d1799e436d555f6ab288917
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-30 14:08:18 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿// Exercises to practice C# Course
+
+//This code calculates the area of a rectangle or quarter
+
+//Declaration of variables
+int sideA;
+int sideB;
+int result;
+
+Console.WriteLine("Rectangle and quarter area calculator");
+Console.WriteLine("Type the side A value: ");
+sideA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type the side B value");
+sideB = Convert.ToInt32(Console.ReadLine());
+
+result = sideA * sideB;
+Console.WriteLine("Side A is: " + sideA + " Side B is: " + " Area = " + result);
+
+
+
+
