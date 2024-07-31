645de99bea74fb6c41893338f1957e81dbf6e9ed
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-31 14:13:32 +0200

    Logic Operators project created and added.

---
+++ LogicOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿//Logic Operators
+
+bool value1 = true;
+bool value2 = true; 
+bool value3 = true;
+
+//Example for AND
+bool resultAnd = value1 && value2 && value3;
+Console.WriteLine(resultAnd);
+
+//Example for OR
+bool resultOr = value1 || value2 || value3;
+Console.WriteLine(resultOr);
+
+
