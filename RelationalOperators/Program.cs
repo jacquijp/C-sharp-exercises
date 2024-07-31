53422d2a8be994f0f69de4bc73b9fd53695995a0
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-31 15:36:33 +0200

    Changes for RelationalOperatos

--- RelationalOperators/Program.cs
+++ RelationalOperators/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//
+
+
+var (value1, value2, value3) = (10, 1, 2);
+
+Console.WriteLine("The values are: " + value1 + value2 + value3);
+
+bool result = value1 == value2;
+Console.WriteLine("The result of the comparition of value1 == value2 is: " +  result);
+
