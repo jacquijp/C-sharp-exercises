5fb55caffb1ca2e7888181d472bf024396c3c41c
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-31 22:10:03 +0200

    adding new conditions

--- ConditionalIF/Program.cs
+++ ConditionalIF/Program.cs
@@ -6,15 +6,22 @@ string message = "";
 
 //BlackJack goal: to gather 21 points with the cards. Or, if less, to have a higher score than the dealer's.
 
-if (totalPlayer > totalDealer)
+if (totalPlayer > totalDealer && totalPlayer < 22)
 {
     message = "You won!! Congratulations nwn";
 }
-else if (totalPlayer <= totalDealer)
+/* else if (totalPlayer <= totalDealer)
 { 
-    message = "You lost. I'm sorry";
+    message = "You lost. I'm sorry uwu";
+} */
+
+else if (totalPlayer >= 22)
+{
+    message = "You're over 22. You've lost";
 }
 else
 {
     message = "Invalid condition";
 }
+
+Console.WriteLine(message);
