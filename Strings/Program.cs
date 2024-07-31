be9e330975c2d57ab6fabe64553fe4a460d254bc
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-31 16:15:19 +0200

    String: Customer personal data added.

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Space to practice Strings
+
+Console.WriteLine("Customer personal data");
+int height = 164;
+int age = 29;
+string name = "Jacqueline Jimenez";
+string originCity = "Guadalajara";
+var bloodType = "B positive";
+
+string presentationCard = $"Information of {name} \n Age: {age} years old. \n Height: {height} cm. \n Origin City: {originCity} . " +
+    $"\n Blood Type: {bloodType";
+
+Console.WriteLine(presentationCard);
+
