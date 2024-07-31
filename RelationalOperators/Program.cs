2f4b5076fcfd67b64182bd5a74a4ad5c3c2a37ad
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-07-31 15:43:18 +0200

    More comparations added

--- RelationalOperators/Program.cs
+++ RelationalOperators/Program.cs
@@ -3,8 +3,18 @@
 
 var (value1, value2, value3) = (10, 1, 2);
 
-Console.WriteLine("The values are: " + value1 + value2 + value3);
+Console.WriteLine("The values are: " + value1 + " - " + value2 + " - " + value3);
 
-bool result = value1 == value2;
-Console.WriteLine("The result of the comparition of value1 == value2 is: " +  result);
+bool resultEqual = value1 == value2;
+Console.WriteLine("The result of the comparition of value1 == value2 is: " +  resultEqual);
+
+bool resultDifferent = value1 != value2;
+Console.WriteLine("The result of the comparition of value1 == value2 is: " + resultDifferent);
+
+bool resultGreaterThan = value1 > value2;
+Console.WriteLine("The result of the comparition of value1 == value2 is: " + resultGreaterThan);
+
+bool resultLessThan = value1 < value2;
+Console.WriteLine("The result of the comparition of value1 == value2 is: " + resultLessThan);
+
 
