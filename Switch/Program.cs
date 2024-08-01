18be588f306fda53c11b0ee2a83297d713ff8713
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-08-01 12:42:58 +0200

    coinsCredit created

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.Design;
 int totalPlayer = 0;
 int totalDealer = 0;
 int num = 0;
+int coinsCredit = 0;
 string message = "";
 string switchControl = "menu";
 
@@ -12,64 +13,73 @@ string switchControl = "menu";
 
 while (true)
 {
-    switch (switchControl)
+    Console.WriteLine("******************************************'");
+    Console.WriteLine("Welcome to BlackJack!");
+    Console.WriteLine("******************************************'");
+    Console.WriteLine("Get credits to play \n 1 credit per game \n " +
+        "How many credits do you want to get?");
+    coinsCredit = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < coinsCredit; i++)
     {
-        case "menu":
-            Console.WriteLine("******************************************'");
-            Console.WriteLine("Welcome to BlackJack!");
-            Console.WriteLine("******************************************'");
-            Console.WriteLine("Select an option (number): ");
-            Console.WriteLine("1 - PLAY \n");
-            switchControl = Console.ReadLine();
-            break;
 
-        case "1":
-            do
-            {
-                System.Random random = new System.Random();
-                num = random.Next(1, 12);
-                totalPlayer = totalPlayer + num;
-                Console.WriteLine("Pick a card");
-                Console.WriteLine($"You got: {num} ");
-                Console.WriteLine("Do you wish another card? (Yes/No)");
-                string answer = Console.ReadLine();
-                if (answer.ToLower() != "yes")
+        switch (switchControl)
+        {
+            case "menu":
 
+                Console.WriteLine("Select an option (number): ");
+                Console.WriteLine("1 - PLAY \n");
+                switchControl = Console.ReadLine();
+                break;
+
+            case "1":
+                do
                 {
-                    break;
+                    System.Random random = new System.Random();
+                    num = random.Next(1, 12);
+                    totalPlayer = totalPlayer + num;
+                    Console.WriteLine("Pick a card");
+                    Console.WriteLine($"You got: {num} ");
+                    Console.WriteLine("Do you wish another card? (Yes/No)");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() != "yes")
+
+                    {
+                        break;
+                    }
+
+                } while (true);
+
+                // Calculate dealer's total after player decides to stop
+                System.Random randomDealer = new System.Random();
+                totalDealer = randomDealer.Next(12, 23);
+
+                Console.WriteLine($"The dealer's total is: {totalDealer}");
+
+                if (totalPlayer > totalDealer && totalPlayer < 22)
+                {
+                    message = "You won!! Congratulations nwn";
                 }
+                else if (totalPlayer > 22)
+                {
+                    message = "You're over 21. You've lost";
+                }
+                else
+                {
+                    message = "You lost";
+                }
+                Console.WriteLine(message);
 
-            } while (true);
+                totalPlayer = 0;
+                totalDealer = 0;
+                switchControl = "menu";
+                break;
 
-            // Calculate dealer's total after player decides to stop
-            System.Random randomDealer = new System.Random();
-            totalDealer = randomDealer.Next(12, 23);
+            default:
+                Console.WriteLine("Type a valid value");
+                switchControl = "menu";
+                break;
+        }
 
-            Console.WriteLine($"The dealer's total is: {totalDealer}");
-
-            if (totalPlayer > totalDealer && totalPlayer < 22)
-            {
-                message = "You won!! Congratulations nwn";
-            }
-            else if (totalPlayer > 22)
-            {
-                message = "You're over 21. You've lost";
-            }
-            else
-            {
-                message = "You lost";
-            }
-            Console.WriteLine(message);
-
-            totalPlayer = 0;
-            totalDealer = 0;
-            switchControl = "menu";
-            break;
-
-        default:
-            Console.WriteLine("Type a valid value");
-            switchControl = "menu";
-            break;
     }
-
 }
