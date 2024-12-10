e332da2f2e03c8695dc6ae453dfb4f0d305761b5
Jacqueline Jimenez Plascencia <jacquelinejp2019@gmail.com>
2024-12-10 18:58:39 +0100

    version

--- POO/pointer.cs
+++ POO/pointer.cs
@@ -3,11 +3,16 @@
 
 pointer pointerPlatzi = new Pointer();
 pointer pointerPlatzi2 = new Pointer();
+
+pointerPlatzi.Color = "Black";
+
+pointerPlatzi2.Color = "White";
+
 class Pointer
 {
-    string Color;
-    double Long;
-    short NumberOfButtons;
+    public string Color;
+    public double Long;
+    public short NumberOfButtons;
     bool
 
 }
